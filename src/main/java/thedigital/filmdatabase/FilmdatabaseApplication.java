9692dc2b8352eed33581c0ebd1924c0c5e3fb0a2
Tkr8kdigital180 <romeo.tagne@skyered-education.de>
2026-04-12 23:35:55 +0200

    first commit

---
+++ src/main/java/thedigital/filmdatabase/FilmdatabaseApplication.java
@@ -0,0 +1,13 @@
+package thedigital.filmdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmdatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thedigital/filmdatabase/FilmdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package thedigital.filmdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
